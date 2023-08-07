e16be018e214620fd0109cbc0336c92b78677a68
waiphyoaung <rubyland.myanmar.mg@gmail.com>
2023-08-07 20:07:02 +0630

    esktop enabled

--- lib/common/widget/navigation_widget.dart
+++ lib/common/widget/navigation_widget.dart
@@ -2,26 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class NavigationWidget extends StatelessWidget {
-  const NavigationWidget({
-    super.key,required  this.shell
-  });
+  const NavigationWidget({super.key, required this.shell});
+
   final StatefulNavigationShell shell;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: shell,
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (index) {
-          shell.goBranch(index);
-        },
-        selectedIndex: shell.currentIndex,
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(
-              icon: Icon(Icons.settings), label: 'Settings'),
+    //layout builder , media query
+    double width = MediaQuery.of(context).size.width;
+    if (width <= 500) {
+      return Scaffold(
+        body: shell,
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (index) {
+            shell.goBranch(index);
+          },
+          selectedIndex: shell.currentIndex,
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.settings), label: 'Settings'),
+          ],
+        ),
+      );
+    } else {
+      return Scaffold(
+          body: Row(
+        children: [
+          NavigationRail(
+            onDestinationSelected: (index) {
+              shell.goBranch(index);
+            },
+            selectedIndex: shell.currentIndex,
+            destinations: const [
+              NavigationRailDestination(
+                  icon: Icon(Icons.home), label: Text('Home')),
+              NavigationRailDestination(
+                  icon: Icon(Icons.settings), label: Text('Settings')),
+            ],
+          ),
+          Expanded(child: shell)
         ],
-      ),
-    );
+      ));
+    }
   }
 }
